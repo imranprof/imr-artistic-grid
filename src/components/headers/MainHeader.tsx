22e99268f6c9a6b73fd6a048605b066d717c0d10
imranprof <imranpers@gmail.com>
2024-12-29 01:04:29 +0600

    chore: add shadcn ui and create sticky header

--- src/components/headers/MainHeader.tsx
+++ src/components/headers/MainHeader.tsx
@@ -1,9 +1,27 @@
+'use client'
+import { cn } from "@/lib/utils"
+
+import { useState, useEffect } from "react"
 import Logo from "@/components/elements/logo/Logo"
 
 
 const MainHeader = () => {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+      const handleScroll = () => {
+          const scrollThreshold = 300;
+          setIsSticky(window.scrollY > scrollThreshold);
+      };
+
+      window.addEventListener("scroll", handleScroll);
+
+      return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
   return (
-    <header>
+    <header className={cn("w-full", isSticky && "!fixed top-0 z-1000 transition-all bg-white h-[80px]")}>
       <div className="container">
         <div className="header_nav h-[100px] flex items-center">
           <Logo />
