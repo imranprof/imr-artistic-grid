b5e625d224d30fa6060a29e01302d04c20062631
imranprof <imranpers@gmail.com>
2025-01-06 23:30:05 +0600

    style(galary): add a grid layout for images

--- src/components/gallery/HomeArtGallery.tsx
+++ src/components/gallery/HomeArtGallery.tsx
@@ -10,15 +10,20 @@ type Props = {
 const HomeArtGallery = ({galleryData}: Props) => {
   console.log('----------',galleryData)
   return (
-    <div>
-      <div className="gallery_wrapper grid grid-cols-3">
+    <div className="">
+      <div className="gallery_wrapper grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
         {galleryData.map((item)=>(
-          <Image key={item.id} alt="img" src={item.path}
-          width={500}
-          height={800}
-          className="w-auto h-auto"
-          priority
-          />
+          <div key={item.id} className="group relative flex justify-center items-center cursor-pointer break-inside-avoid bg-white p-4 rounded-xl">
+            <Image  alt="img" src={item.path}
+            width={400}
+            height={0}
+
+            className="rounded-xl w-full h-auto"
+            style={{objectFit:"cover"}}
+            priority
+            />
+            <div className="absolute w-full h-full bg-black opacity-0 group-hover:opacity-20 rounded-xl transition-all duration-300"></div>
+          </div>
         ))}
       </div>
     </div>
